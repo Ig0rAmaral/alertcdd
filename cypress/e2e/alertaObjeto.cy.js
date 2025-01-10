2216c821a7ae87ee05edc4a6a335f21099e2f58c
igorsantana <863690215@ssp.ba.intranet>
2025-01-10 09:05:56 -0300

    Commit inicial

--- cypress/e2e/alertaObjeto.cy.js
+++ cypress/e2e/alertaObjeto.cy.js
@@ -5,7 +5,7 @@ beforeEach('' , () => {
     cy.login_com_sucesso();
     Cypress.on('uncaught:exception', (err, runnable) => {
         return false
-    })
+})
 });
 describe('Alerta cidadão - Emissão de alerta ao objeto ', () => {
     it('CT001 - Emitir alerta ao objeto com sucesso', () => {
